4022c2fe76a875ee7b6e7638297df6ee5cf96b62
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-31 15:03:20 -0800

    working on serializer model and view

---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,9 @@
+"""Create a serializer to transform snippets into JSON representations."""
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
