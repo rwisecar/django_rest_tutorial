6405b34f8693f59ba0908b8475581ee27eb3bc70
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-02-04 12:12:53 -0800

    rewriting serializer to use hyperlinked model serializer, enabling highlighting in views and urls

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework import renderers
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
@@ -39,3 +44,11 @@ class UserDetail(generics.RetrieveAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+        })
